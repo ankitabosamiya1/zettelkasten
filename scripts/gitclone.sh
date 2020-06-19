254d817778d2c22bae5215237d4e1509ca4b4d64
ankita <ankitadhandha@gmail.com>
2020-06-20 00:34:31 +0530

    Edited gitclone file.

--- scripts/gitclone.sh
+++ scripts/gitclone.sh
@@ -3,9 +3,10 @@ rm -rf zettelkasten
 git clone https://github.com/ankitadhandha/zettelkasten
 cd zettelkasten
 chmod -R 777 .
-neuron rib -w
+neuron rib
 ls
 pwd
 cd .neuron/output
 aws s3 cp . s3://common-neuron/ --recursive --exclude ".git*" --exclude ".neuron*" --exclude "scripts"
+
 
