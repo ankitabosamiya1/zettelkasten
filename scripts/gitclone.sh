b833eebd46ccf12c0cfa3ce9a42d6543923cb479
ankita <ankitadhandha@gmail.com>
2020-06-20 01:02:56 +0530

    Edited appspec.yml file.

--- scripts/gitclone.sh
+++ scripts/gitclone.sh
@@ -4,9 +4,5 @@ git clone https://github.com/ankitadhandha/zettelkasten
 cd zettelkasten
 chmod -R 777 .
 neuron rib -w
-ls
-pwd
-cd .neuron/output
-aws s3 cp . s3://common-neuron/ --recursive --exclude ".git*" --exclude ".neuron*" --exclude "scripts" 
 
 
---
+++ scripts/neuron.sh
@@ -0,0 +1,2 @@
+cd /home/ec2-user/zettelkasten/.neuron/output
+aws s3 cp . s3://common-neuron/ --recursive --exclude ".git*" --exclude ".neuron*" --exclude "scripts"
