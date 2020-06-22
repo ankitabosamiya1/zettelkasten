9ba101a234eaae1c27556e205563770bf6306e4a
ankita <ankitadhandha@gmail.com>
2020-06-22 21:50:17 +0530

    Removed neuron.sh.

--- scripts/neuron.sh
+++
@@ -1,5 +0,0 @@
-#cd /home/ec2-user/zettelkasten
-#neuron rib 
-#cd /home/ec2-user/zettelkasten/.neuron/output
-#chmod -R 777 .
-#aws s3 cp . s3://common-neuron/ --recursive --exclude ".git*" --exclude ".neuron*" --exclude "scripts" 
