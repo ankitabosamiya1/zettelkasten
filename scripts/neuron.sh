dc25c84eab39bc02d81ecac5673423207bce390f
ankita <ankitadhandha@gmail.com>
2020-06-22 21:29:10 +0530

    Edited script on 22/06.

--- scripts/neuron.sh
+++ scripts/neuron.sh
@@ -1,4 +1,5 @@
 cd /home/ec2-user/zettelkasten
 neuron rib 
 cd /home/ec2-user/zettelkasten/.neuron/output
+chmod -R 777 .
 aws s3 cp . s3://common-neuron/ --recursive --exclude ".git*" --exclude ".neuron*" --exclude "scripts" 
