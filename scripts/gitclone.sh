8a88f438f56a012e0f218c43a8e0283da5c02c18
ankita <ankitadhandha@gmail.com>
2020-06-23 18:33:53 +0530

    Testing on 23/06/20.

--- scripts/gitclone.sh
+++ scripts/gitclone.sh
@@ -1,6 +1,6 @@
-cd /home/ec2-user
-rm -rf zettelkasten
-git clone https://github.com/ankitadhandha/zettelkasten
+#cd /home/ec2-user
+#rm -rf zettelkasten
+#git clone https://github.com/ankitadhandha/zettelkasten
 cd zettelkasten
 chmod -R 777 .
 
